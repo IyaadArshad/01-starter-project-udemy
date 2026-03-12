501a250f044b9cbcb1b7a39272dfe178e462f3aa
IyaadArshad <hello@iyaadarshad.com>
2026-03-12 08:02:27 +0400

    update for lecture 47

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,6 @@
-import reactImage from "./assets/react-core-concepts.png";
-import componentsImage from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"]
-
-function generateRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[generateRandomInt(3)]} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title + " image"} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  )
-}
+import { Header } from "./components/Header";
+import { CoreConcept } from "./components/CoreConcept";
 
 function App() {
   return (
@@ -43,7 +14,7 @@ function App() {
               <CoreConcept
                 title={CORE_CONCEPTS[0].title}
                 description={CORE_CONCEPTS[0].description}
-                image={CORE_CONCEPTS[0].image}/>
+                image={CORE_CONCEPTS[0].image} />
               <CoreConcept
                 title={CORE_CONCEPTS[1].title}
                 description={CORE_CONCEPTS[1].description}
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import reactImage from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"]
+
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+export function Header() {
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions[generateRandomInt(3)]} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
