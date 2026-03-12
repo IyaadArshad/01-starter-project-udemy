0a8eb602941ad3af7ad611feae0c68425e4a1c98
IyaadArshad <hello@iyaadarshad.com>
2026-03-12 07:45:19 +0400

    update for lecture 43

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImage from "./assets/react-core-concepts.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"]
 
 function generateRandomInt(max) {
@@ -7,7 +9,7 @@ function generateRandomInt(max) {
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {reactDescriptions[generateRandomInt(3)]} React concepts you will need for almost any app you are
