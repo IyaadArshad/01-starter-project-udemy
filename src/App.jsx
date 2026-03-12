2e9c7080982ed8c9ba5fc392b4a0feb8b08bc7b1
IyaadArshad <hello@iyaadarshad.com>
2026-03-12 07:58:48 +0400

    updates for lecture 45

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactImage from "./assets/react-core-concepts.png";
 import componentsImage from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"]
 
@@ -20,7 +21,7 @@ function Header() {
   );
 }
 
-function CoreConcept (props) {
+function CoreConcept(props) {
   return (
     <li>
       <img src={props.image} alt={props.title + " image"} />
@@ -39,10 +40,22 @@ function App() {
           <section id="core-concepts">
             <h2>core concepts</h2>
             <ul>
-              <CoreConcept title="Components" description="The core UI building block" image={componentsImage}/>
-              <CoreConcept />
-              <CoreConcept />
-              <CoreConcept />
+              <CoreConcept
+                title={CORE_CONCEPTS[0].title}
+                description={CORE_CONCEPTS[0].description}
+                image={CORE_CONCEPTS[0].image}/>
+              <CoreConcept
+                title={CORE_CONCEPTS[1].title}
+                description={CORE_CONCEPTS[1].description}
+                image={CORE_CONCEPTS[1].image} />
+              <CoreConcept
+                title={CORE_CONCEPTS[2].title}
+                description={CORE_CONCEPTS[2].description}
+                image={CORE_CONCEPTS[2].image} />
+              <CoreConcept
+                title={CORE_CONCEPTS[3].title}
+                description={CORE_CONCEPTS[3].description}
+                image={CORE_CONCEPTS[3].image} />
             </ul>
           </section>
           <h2>Time to get started!</h2>
