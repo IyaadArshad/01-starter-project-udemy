79cc8c7d5b05b5b1466c241fe09abf96c974e453
IyaadArshad <hello@iyaadarshad.com>
2026-03-12 07:42:57 +0400

    update for lecture 42

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"]
+
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {reactDescriptions[generateRandomInt(3)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
+
 function App() {
   return (
     <div>
