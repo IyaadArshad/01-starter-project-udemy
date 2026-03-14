9bb32b93d80548036e24b20439129689b55cb51a
IyaadArshad <hello@iyaadarshad.com>
2026-03-14 08:22:46 +0400

    update for lecture 55

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { Header } from "./components/Header/Header";
 import { CoreConcept } from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 import { useState } from 'react';
+import { EXAMPLES } from "./data";
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState("please click a button");
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton)
@@ -41,13 +42,25 @@ function App() {
           <section id="examples">
             <h2>Examples</h2>
             <menu>
-              <TabButton onSelect={() => handleSelect('component')}>Component</TabButton>
+              <TabButton onSelect={() => handleSelect('components')}>Component</TabButton>
               <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
               <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-              <TabButton onSelect={() =>handleSelect('state')}>State</TabButton>
+              <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
             </menu>
           </section>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>
+              {EXAMPLES[selectedTopic].title}
+            </h3>
+            <p>
+              {EXAMPLES[selectedTopic].description}
+            </p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </main>
       </div>
     </div>
